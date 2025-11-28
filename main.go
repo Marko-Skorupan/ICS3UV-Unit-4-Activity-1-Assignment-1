6b4ff949252656434b2db14346830aea3fbeb6c5
default <marko.skorupan@stu.ocsb.ca>
2025-11-28 23:42:42 +0000

    User enters marks and receives correct average + performance

--- main.go
+++ main.go
@@ -1,1 +1,8 @@
-package ics3uvunit4activity1assignment1
+/*
+ * @author Marko Skorupan
+ * @version 1.0.0
+ * @date 2025-11-27
+ * @fileoverview User enters marks and receives correct average + performance.
+ */
+
+package main
